4c7e0fbb1cd0c2d90a5c669649844cdd700da60c
Guillaume Pelletier <guillaume.pelletier@dotvision.com>
2021-10-01 12:57:54 +0200

    cleaning

--- src/FFTPanel.tsx
+++ src/FFTPanel.tsx
@@ -6,11 +6,7 @@ import { FFTOptions } from "types";
 // highcharts
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-<<<<<<< HEAD
 import { ForwardFFTPanelData, IFFTResult} from "FFTPanelData";
-=======
-import { ForwardFFTPanelData } from "FFTPanelData";
->>>>>>> 248cd7879e1441cd7f4977bbe912ede0803d7db7
 
 // tslint:disable-next-line: interface-name
 interface Props extends PanelProps<FFTOptions> {}
@@ -21,18 +17,13 @@ export class FFTPanel extends PureComponent<Props> {
 
   constructor(props: Readonly<Props>) {
     super(props);
-<<<<<<< HEAD
     const {serie,sampling} = props.options ;
     this._fft = new ForwardFFTPanelData(serie, sampling);
-=======
-    this._fft = new ForwardFFTPanelData(props.options.serie);
->>>>>>> 248cd7879e1441cd7f4977bbe912ede0803d7db7
   }
 
   // tslint:disable: typedef
   render() {
     const { data, options } = this.props;
-<<<<<<< HEAD
     var chartdata : any = [];
     try {
       this._fft.write(data);
@@ -48,23 +39,6 @@ export class FFTPanel extends PureComponent<Props> {
     } catch (error) {
       console.log("Error:" + error );
     }
-=======
-    var chunk : any ;
-    try {
-      this._fft.write(data);
-      chunk = this._fft.read();
-      // tslint:disable-next-line: no-empty
-    } catch {
-    }
-
-    var spectrum:number[] = [];
-    if(chunk) {
-      for(let i:number=2;i<chunk.length;i+=2) {
-        spectrum.push(chunk[i]);
-      }
-    }
-
->>>>>>> 248cd7879e1441cd7f4977bbe912ede0803d7db7
     const chartOptions = {
         chart:{
           backgroundColor: "rgba(0,0,0,0)"
--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,14 @@
 // tslint:disable-next-line: interface-name
 export interface FFTOptions {
   serie: string;
-<<<<<<< HEAD
   sampling : number;
-=======
->>>>>>> 248cd7879e1441cd7f4977bbe912ede0803d7db7
 }
 
  export const defaults: FFTOptions = {
    serie: "put your serie name here.",
-<<<<<<< HEAD
    sampling : 1
  };
 
-=======
- };
-
-/** Time/interval measured in samples. */
-export type Smp = number;
-
->>>>>>> 248cd7879e1441cd7f4977bbe912ede0803d7db7
 /** Time/interval measured in seconds. */
 export type Seconds = number;
 
